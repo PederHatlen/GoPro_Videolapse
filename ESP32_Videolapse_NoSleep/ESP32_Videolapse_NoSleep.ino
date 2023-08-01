7f2ab5989bbbb192b6d1600f9e60c6f361571c11
Peder <pederhatlen@gmail.com>
2023-08-01 14:08:28 +0200

    More debug logging, different time transfer

--- ESP32_Videolapse_NoSleep/ESP32_Videolapse_NoSleep.ino
+++ ESP32_Videolapse_NoSleep/ESP32_Videolapse_NoSleep.ino
@@ -10,48 +10,70 @@
 
 #define RPI_WAIT  60
 
+#define SECOND 1000
+
 CRGB leds[LED_NUM];
 
 void setup() {
-  /* Start power to camera system */
   pinMode(relay_pin, OUTPUT);
 
   /* Initiate RGB Led */
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, LED_NUM);
 
-  /* Connect serial -> wait for connection -> send "hello i exist" */
-
   USBSerial.begin();
 }
 
 void loop() {
+  /* Connect serial -> wait for connection -> send "hello i exist" */
   Serial1.begin(9600, SERIAL_8N1, RX1, TX1);
 
+  /* Start LED */
   leds[0] = CRGB::Red;
   FastLED.show();
 
+  /* Start power to camera system */
   digitalWrite(relay_pin, HIGH);
 
-  /* Wait untill rpi sends sleep time */
-
   long start_millis = millis();
-
   long timeout = 0;
 
+  /* Wait for connection to serial */
   while (Serial1.available() == 0){
     delay(1000);
     if ((millis() - start_millis) > 600000) timeout = 70;
   }
 
-  USBSerial.println((String)"RaspberryPi has booted after "+String((millis()-start_millis)/1000)+" seconds");
+  USBSerial.println((String)"RaspberryPi has booted after "+String((millis()-start_millis)/SECOND)+" seconds");
+
+  String serial_buffer;
   
+  /* Wait untill rpi sends sleep time */
   while (timeout == 0){
-    delay(1000);
-    Serial1.println("Waiting for data");
-    USBSerial.println("Waiting for data");
-    timeout = Serial1.parseInt();
+    delay(SECOND);
+    // Serial1.println("Waiting for data");
+    if (Serial1.available() > 0) {
+      /* Atempt better data transfer (doesn't really work when getting trash over serial)*/
+      USBSerial.println("Data incoming");
+      for(int i = 0; i < Serial1.available(); i++){
+        char c = Serial1.read();
+        serial_buffer += c;
+        USBSerial.println(c);
+      }
+      /* Finding start and end strings of message */
+      int index_start = serial_buffer.indexOf("Sleep for ");
+      int index_end = serial_buffer.indexOf(" seconds");
+
+      USBSerial.println(serial_buffer.substring(index_start+10, index_end));
+      
+      if(index_start >= 0 && index_end >= 0){
+        timeout = serial_buffer.substring(index_start+10, index_end).toInt();
+      }
+    }
+
+    // timeout = Serial1.parseInt();
+
     // timeout = USBSerial.parseInt();
-    if ((millis() - start_millis) > 600000) timeout = 70;
+    if ((millis() - start_millis) > (600 * SECOND)) timeout = 70;
   }
 
   long final_sleep = (timeout <= (RPI_WAIT+10))? 10:(timeout-RPI_WAIT);
@@ -59,15 +81,34 @@ void loop() {
   leds[0] = CRGB::Blue;
   FastLED.show();
 
-  Serial1.println((String)"Seconds untill wakeup: "+String(final_sleep+60));
-  USBSerial.println((String)"Seconds untill wakeup: "+String(final_sleep+60));
+  Serial1.println((String)"Seconds untill wakeup: "+String(final_sleep+RPI_WAIT));
+  USBSerial.println((String)"Seconds untill wakeup: "+String(final_sleep+RPI_WAIT) + ", Got "+String(timeout)+" from RPI");
 
   /* Delay for 1 minute, ensure safe raspi shutdown */
-  delay(RPI_WAIT * 1000);
+  delay(RPI_WAIT * SECOND);
 
   FastLED.clear(true);
 
   digitalWrite(relay_pin, LOW);
 
-  delay(final_sleep * 1000);
+  const int delay_chunk = 10;
+
+  // serial_buffer = "";
+
+  for(int i = 0; i <= floor(final_sleep/delay_chunk); i++){
+    // if (USBSerial.available() > 0) {
+    //   for(int i = 0; i < USBSerial.available(); i++){serial_buffer += USBSerial.read();}
+    //   if(serial_buffer.indexOf("Force the system to start") >= 0){
+    //     USBSerial.println("Starting by request from USB");
+    //     return;
+    //   }
+    // }
+
+    USBSerial.println((String)String(final_sleep-(delay_chunk*i)) + " seconds left untill wakeup");
+    delay(delay_chunk * SECOND);
+  }
+
+  // USBSerial.println((String)String(final_sleep%delay_chunk) + " seconds left untill wakeup");
+
+  delay((final_sleep%delay_chunk) * SECOND);
 }
