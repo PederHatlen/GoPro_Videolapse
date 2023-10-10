f2ed906bb219ef431dc6122592b2bbc9ec0afd15
Peder <pederhatlen@gmail.com>
2023-10-10 18:16:03 +0200

    Get latest version from github

---
+++ setup/getLatest.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+serverIP="192.168.1.39:5500"
+
+rm -f /home/nrkbeta/VideoLapse.py
+wget "https://raw.github.com/PederHatlen/GoPro_Videolapse/main/VideoLapse.py" -O /home/nrkbeta/VideoLapse.py
